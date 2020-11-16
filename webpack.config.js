7f20c28ebbf06ce61ae529f8dd61fdc6f16f2a99
lavikara <karatemitayo@gmail.com>
2020-11-17 00:26:06 +0100

    setup project with webpack

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: ["./src/js/index.js", "./src/assets/main.scss"],
+
+  output: {
+    filename: "[name].[contenthash:8].js",
+    path: path.resolve(__dirname, "dist"),
+    chunkFilename: "[name].[contenthash:8].js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)(\?\S*)?$/,
+        loader: "file-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif|webm|mp4|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name][contenthash:8].[ext]",
+          outputPath: "assets/img",
+          esModule: false,
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash:8].css",
+      chunkFilename: "[name].[contenthash:8].css",
+    }),
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+      favicon: "./public/favicon.svg",
+    }),
+    new Dotenv({
+      path: "./.env",
+      safe: true,
+    }),
+  ],
+
+  optimization: {
+    moduleIds: "hashed",
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          priority: -10,
+          chunks: "all",
+        },
+      },
+    },
+  },
+
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    historyApiFallback: true,
+  },
+};
